6c11da9cc1baef116ca1c6a6ed65ae23700244e5
Elias Geiger <elias@ele-dev.de>
2022-02-14 19:19:48 +0100

    Video Clip OOP - intial approach

--- web-service/webroot/php/pullVideoList.php
+++ web-service/webroot/php/pullVideoList.php
@@ -11,43 +11,32 @@
 	echo "<H1>Aufnahmen:</H1><table><tbody>";
 	echo "<tr><th>Wochentag</th><th>Datum</th><th>Beginn</th><th>Dateigröße</th><th>Anschauen</th><th>Herunterladen</th></tr>";
 
-	// Function to generate Weekday names from indexes
-	function getWeekdayStr($dayIdx)
+	// Get file path to recorded streams and create the clip objects
+	$vid_filePaths = glob("/var/www/html/videos/*.mp4");
+	for($i = 0; $i < count($vid_filePaths); $i++)
 	{
-		if($dayIdx == "%0")
-			return "Sonntag";
-		else if($dayIdx == "%1")
-			return "Montag";
-		else if($dayIdx == "%2")
-			return "Dienstag";
-		else if($dayIdx == "%3")
-			return "Mittwoch";
-		else if($dayIdx == "%4")
-			return "Donnerstag";
-		else if($dayIdx == "%5")
-			return "Freitag";
-		else 
-			return "Samstag";
+		$vid_clips[$i] = new VideoClip($vid_filePaths[$i]);
+		// var_dump($vid_clips[$i]);
 	}
 
-	// Get file path to recorded streams
-	$vid_filePaths = glob("/var/www/html/videos/*.mp4");
-	for($i = 0; $i < count($vid_filePaths); $i++) {
-		$temp = explode(".", $vid_filePaths[$i]);
-		$recordDate = str_replace("-", ".", $temp[1]);
-		$recordTime = str_replace("-", ":", $temp[2]);
-		$weekDay = getWeekdayStr(date("%w", strtotime($recordDate)));
-		// echo " " . $weekDay;
-		if(($i+2) % 2 != 0) {
-            echo "<tr class='white'><td>" . $weekDay . "</td><td>" . $recordDate . "</td><td>" . $recordTime . "</td><td>" 
-                . round(filesize($vid_filePaths[$i])/1000000) . " MB</td><td><a href='" . htmlspecialchars("videos/" . basename($vid_filePaths[$i])) 
+	for($j = 0; $j < count($vid_clips); $j++)
+	{
+		// current file
+		$file = $vid_clips[$j];
+		if(($j+2) % 2 != 0) {
+            echo "<tr class='white'><td>" . $file->getWeekday() . "</td><td>" 
+				. $file->getRecordDate() . "</td><td>" . $file->getRecordTime() . "</td><td>" 
+                . $file->getFilesize() . " MB</td><td><a href='" . htmlspecialchars("videos/" . $file->getFilename()) 
                 . "'><i class='fas fa-play-circle' style='color:black;font-size:23px;'></i></a></td>
-                <td><a href='" . htmlspecialchars("videos/" . basename($vid_filePaths[$i])) . "' download='video'><i class='fas fa-download' style='color:black;font-size:23px;'></i></a></td></tr>";
+                <td><a href='" . htmlspecialchars("videos/" . $file->getFilename()) 
+				. "' download='video'><i class='fas fa-download' style='color:black;font-size:23px;'></i></a></td></tr>";
 		} else {
-			echo "<tr class='grey'><td>" . $weekDay . "</td><td>" . $recordDate . "</td><td>" . $recordTime . "</td><td>" 
-			. round(filesize($vid_filePaths[$i])/1000000) . " MB</td><td><a href='" . htmlspecialchars("videos/" . basename($vid_filePaths[$i])) 
+			echo "<tr class='grey'><td>" . $file->getWeekday() . "</td><td>" 
+			. $file->getRecordDate() . "</td><td>" . $file->getRecordTime() . "</td><td>" 
+			. $file->getFilesize() . " MB</td><td><a href='" . htmlspecialchars("videos/" . $file->getFilename()) 
 			. "'><i class='fas fa-play-circle' style='color:black;font-size:23px;'></i></a></td>
-			<td><a href='" . htmlspecialchars("videos/" . basename($vid_filePaths[$i])) . "' download='video'><i class='fas fa-download' style='color:black;font-size:23px;'></i></a></td></tr>";
+			<td><a href='" . htmlspecialchars("videos/" . $file->getFilename()) 
+			. "' download='video'><i class='fas fa-download' style='color:black;font-size:23px;'></i></a></td></tr>";
 		}
 	}
 
--- web-service/webroot/php/videoClipClass.php
+++ web-service/webroot/php/videoClipClass.php
@@ -10,15 +10,66 @@
     class VideoClip
     {
         private $filename = "";
+        private $dateStr = "";
+        private $timeStr = "";
+        private $weekDay = "";
+        private $fileSize = 0;
 
-        public function __construct($filename)
+        public function __construct($path)
         {
-            $this->filename = $filename;
+            $this->filename = basename($path);
+            // extract date and time from filename and store it
+            $temp = explode(".", $path);
+            $this->dateStr = str_replace("-", ".", $temp[1]);
+            $this->timeStr = str_replace("-", ":", $temp[2]);
+
+            // also determine the weekday of the date and the filesize
+            $this->weekDay = VideoClip::getWeekdayStr(date("%w", strtotime($this->dateStr)));
+            $this->fileSize = round(filesize($path) / 1000000);
         }
 
         public function getFilename()
         {
             return $this->filename;
+        }
+
+        public function getRecordDate()
+        {
+            return $this->dateStr;
+        }
+
+        public function getRecordTime()
+        {
+            return $this->timeStr;
+        }
+
+        public function getWeekday()
+        {
+            return $this->weekDay;
+        }
+
+        public function getFilesize()
+        {
+            return $this->fileSize;
+        }
+
+        // Function to generate Weekday names from indexes
+        public static function getWeekdayStr($dayIdx)
+        {
+            if($dayIdx == "%0")
+                return "Sonntag";
+            else if($dayIdx == "%1")
+                return "Montag";
+            else if($dayIdx == "%2")
+                return "Dienstag";
+            else if($dayIdx == "%3")
+                return "Mittwoch";
+            else if($dayIdx == "%4")
+                return "Donnerstag";
+            else if($dayIdx == "%5")
+                return "Freitag";
+            else 
+                return "Samstag";
         }
     }
 
