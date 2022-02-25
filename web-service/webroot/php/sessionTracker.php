ac78f8cd4a9d7534d1059faa56291f963b72141a
Elias Geiger <elias@ele-dev.de>
2022-02-25 01:17:30 +0100

    Improved session tracking

--- web-service/webroot/php/sessionTracker.php
+++ web-service/webroot/php/sessionTracker.php
@@ -16,19 +16,24 @@
             // check if already tracked, if not add an entry
             $query = "SELECT * FROM tbl_viewerSession WHERE id LIKE '" . htmlspecialchars(session_id()) . "'";
             if(mysqli_num_rows(mysqli_query($handle, $query)) == 0) {
-                $insertQuery = "INSERT INTO tbl_viewerSession (id, time_stamp) VALUES ('" . htmlspecialchars(session_id()) . "', 'blank')";
+                $insertQuery = "INSERT INTO tbl_viewerSession (id, lastTime) VALUES ('" . htmlspecialchars(session_id()) . "', CURRENT_TIMESTAMP())";
                 $result = mysqli_query($handle, $insertQuery);
             } else {
                 // when already tracked, renew the timestamp
-                // ...
+                $updateQuery = "UPDATE tbl_viewerSession SET lastTime = CURRENT_TIMESTAMP() WHERE id LIKE '" . htmlspecialchars(session_id()) . "'";
+                $result = mysqli_query($handle, $updateQuery);
             }
         }
         else
         {
-            // Delete tracking entry from the list, additionally delete old sessions (likely inactive => timed out)
+            // Delete tracking entry from the list
             $delQuery = "DELETE FROM tbl_viewerSession WHERE id LIKE '" . htmlspecialchars(session_id()) . "'";
             $result = mysqli_query($handle, $delQuery);
         }
+
+        // Additionally delete old session entries (inactive for > 1 min)
+        $delQuery = "DELETE FROM tbl_viewerSession WHERE lastTime < CURRENT_TIMESTAMP() - 150000";
+        $result = mysqli_query($handle, $delQuery);
 
         // Finally close DB connection
         mysqli_close($handle);
